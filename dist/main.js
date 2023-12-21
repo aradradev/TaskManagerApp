75c5a2781bc6269d998d7512e21d5d187f280d4b
Abdoul <abdoulramanediallo44@gmail.com>
2023-12-21 17:52:19 +0000

    compiling the type script

--- dist/main.js
+++ dist/main.js
@@ -1,35 +1,4 @@
 "use strict";
-//src/main.ts
-class TaskManager {
-    constructor() {
-        this.tasks = [];
-    }
-    addTask(task) {
-        this.tasks.push(task);
-    }
-    editTask(taskId, updatedTask) {
-        const taskIndex = this.tasks.findIndex((task) => task.id === taskId);
-        if (taskIndex !== -1) {
-            this.tasks[taskIndex] = Object.assign(Object.assign({}, this.tasks[taskIndex]), updatedTask);
-        }
-    }
-    deleteTask(taskId) {
-        this.tasks = this.tasks.filter((task) => task.id !== taskId);
-    }
-    getTask() {
-        return this.tasks;
-    }
-}
-const exampleTask = {
-    id: 1,
-    title: 'Sample Title',
-    priority: 'medium',
-    dueDate: new Date('2023-12-31'),
-    completed: false,
-};
-const taskManager = new TaskManager();
-taskManager.addTask(exampleTask);
-taskManager.editTask(1, { completed: true });
-console.log(taskManager.getTask());
-taskManager.deleteTask(1);
-// console.log(taskManager.getTask())
+Object.defineProperty(exports, "__esModule", { value: true });
+const user_interface_1 = require("./user-interface");
+(0, user_interface_1.runTaskManager)(user_interface_1.taskManager);
