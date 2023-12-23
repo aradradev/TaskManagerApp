abf41bced65d76185516695041a578ef1172016d
Abdoul <abdoulramanediallo44@gmail.com>
2023-12-23 06:15:19 +0000

    adding user interface

--- dist/user-interface.js
+++ dist/user-interface.js
@@ -63,12 +63,14 @@ exports.taskManager = new task_manager_1.TaskManager();
 function runTaskManager(taskManager) {
     while (true) {
         console.log('\n Task Manager Menu:');
-        console.log('1. Add Task');
-        console.log('2. Edit Task');
-        console.log('3. Delete Task');
-        console.log('4. View Tasks');
-        console.log('5. Sort Tasks');
-        console.log('6. Exit');
+        console.log('[1]. Add Task');
+        console.log('[2]. Edit Task');
+        console.log('[3]. Delete Task');
+        console.log('[4]. View Tasks');
+        console.log('[5]. Sort Tasks');
+        console.log('[6]. Save Tasks to File');
+        console.log('[7]. Load Tasks from File');
+        console.log('[8]. Exit');
         const choice = readlineSync.keyIn('Enter your choice: ');
         switch (choice) {
             case '1':
@@ -100,6 +102,12 @@ function runTaskManager(taskManager) {
                 }
                 break;
             case '6':
+                taskManager.saveTasksToFile();
+                break;
+            case '7':
+                taskManager.loadTasksFromFile();
+                break;
+            case '8':
                 console.log('Thank you, have a good time. Bye!');
                 process.exit(0);
             default:
