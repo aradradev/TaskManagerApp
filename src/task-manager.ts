ee5326f9c191efd067e42dc56a9d631abc158e8b
Abdoul <abdoulramanediallo44@gmail.com>
2023-12-26 07:52:04 +0000

    adding task management

--- src/task-manager.ts
+++ src/task-manager.ts
@@ -2,6 +2,7 @@
 import chalk from 'chalk';
 import * as fs from 'fs'
 import * as path from 'path'
+import * as notifier from 'node-notifier'
 
 export interface Task{
     id: number;
@@ -14,21 +15,31 @@ export interface Task{
 
 export class TaskManager {
     private tasks: Task[] = []
+    private undoStack: Task[][] = []
+    private redoStack: Task[][] = []
 
     addTask(task: Task): void{
+        this.pushToUndoStack();
         this.tasks.push(task)
+        this.pushToRedoStack()
     }
 
     editTask(taskId: number, updatedTask: Partial<Task>): void{
+        this.pushToUndoStack()
+
         const taskIndex = this.tasks.findIndex((task) => task.id === taskId)
 
         if (taskIndex !== -1) {
             this.tasks[taskIndex] = {...this.tasks[taskIndex], ...updatedTask}
         }
+
+        this.pushToRedoStack()
     }
 
     deleteTask(taskId: number): void{
+        this.pushToUndoStack()
         this.tasks = this.tasks.filter((task) => task.id !== taskId)
+        this.pushToRedoStack()
     }
 
     getTasks(): Task[] {
@@ -85,6 +96,93 @@ export class TaskManager {
             }
         } catch (error: any) {
             console.error(`${chalk.red(`Error loading tasks from ${filePath}: ${error.message}`)}`)
+        }
+    }
+
+    //add new method to get current state
+    getCurrentState(): Task[] {
+        return [...this.tasks]
+    }
+
+    //add new method to push the current state to undo stack
+    private pushToUndoStack(): void{
+        this.undoStack.push(this.getCurrentState())
+    }
+
+    //add method to push the current state to redo stack
+    private pushToRedoStack(): void{
+        this.redoStack.push(this.getCurrentState())
+    }
+
+    //add method to pop state from the undo stack
+    private popFromUndoStack(): Task[] | undefined {
+        return this.undoStack.pop()
+    }
+
+    //add method to pop state from the redi stack
+    private popFromRedoStack(): Task[] | undefined {
+        return this.redoStack.pop()
+    }
+
+    //Implement Undo methods
+    undo(): void{
+        if (this.undoStack.length > 0) {
+            const prevState = this.popFromUndoStack()
+            if (prevState) {
+                this.tasks = prevState
+                this.pushToRedoStack()
+            }
+        }
+    }
+
+    //Implement Redo methods
+    redo(): void{
+        if (this.redoStack.length > 0) {
+            const nextState = this.popFromRedoStack()
+            if (nextState) {
+                this.tasks = nextState
+                this.pushToUndoStack()
+            }
+        }
+    }
+
+    //Add reminder method: I have used notifier from node
+    checkDueDateReminders(): void{
+        const today = new Date()
+
+        for (const task of this.tasks) {
+            if (task.dueDate && this.isDueDateApproaching(task.dueDate, today)) {
+                notifier.notify({
+                    title: 'Task Reminder',
+                    message: `Task "${task.title}" is approaching its due date!`
+                })
+                
+            }
+        }
+    }
+
+    private isDueDateApproaching(dueDate: Date, today: Date): boolean{
+        const daysBeforeReminder = 2
+        const timeDifference = dueDate.getTime() - today.getTime()
+        const daysDifference = timeDifference / (1000 * 3600 * 24)
+        return daysDifference <= daysBeforeReminder
+    }
+
+    //Add new method to Task Manager Filtering features
+    filterTasks(criteria: 'category' | 'priority' | 'dueDate' | 'completed'): Task[] {
+        switch (criteria) {
+            case 'category':
+                return this.tasks.filter((task) => task.category === 'work')
+            case 'priority':
+                return this.tasks.filter((task) => task.priority === 'high')
+            case 'dueDate':
+                return this.tasks.filter((task) => task.dueDate && task.dueDate.getTime() < new Date().getTime())
+            case 'completed':
+                return this.tasks.filter(task => task.completed)
+            default:
+                console.log(chalk.red('Invalid filtering criterion.'));
+                return []
+                
         }
     }
 
